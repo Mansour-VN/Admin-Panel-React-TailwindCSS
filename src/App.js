626458ed1054c592cd1f924352c1000f713f5a74
Mansour.vn <67545478+Mansour-VN@users.noreply.github.com>
2022-11-13 23:25:34 +0330

    update app,index,sidebar and create context file

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+
+import { useStateContext } from "./context/ContextProvider";
 import "./App.css";
 
 function App() {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
 
   return (
     <div>
