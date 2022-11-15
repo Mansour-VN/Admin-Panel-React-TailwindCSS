aada88d0255c4f0b8f3d34f1277d252b50fbc68d
Mansour.vn <67545478+Mansour-VN@users.noreply.github.com>
2022-11-15 12:59:26 +0330

    Make navbar and edit sidebar and context

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,24 +1,42 @@
-import React , {createContext, useContext, useState} from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
-const initialState ={
-    chat: false,
-    cart: false,
-    userProfile: false,
-    Notification: false,
-}
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
-const ContextProvider = ({children}) => {
-    const [activeMenu,setActiveMenu] = useState(true);
+const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
 
   return (
-    <StateContext.Provider value={{activeMenu,setActiveMenu}}>
-        {children}
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+        screenSize,
+        setScreenSize,
+      }}
+    >
+      {children}
     </StateContext.Provider>
-  )
-}
+  );
+};
 
-export const useStateContext = () =>useContext(StateContext);
+export const useStateContext = () => useContext(StateContext);
 
-export default ContextProvider
+export default ContextProvider;
